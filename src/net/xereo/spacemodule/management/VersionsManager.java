3e6d17b90ea41bc2b14a117cb6a49c1623bfe85e
drdanick <>
2012-04-07 02:16:36 +1200

    Refactor: me.neatmonster --> net.xereo

--- src/net/xereo/spacemodule/management/VersionsManager.java
+++ src/net/xereo/spacemodule/management/VersionsManager.java
@@ -1,24 +1,24 @@
 /*
  * This file is part of SpaceModule (http://spacebukkit.xereo.net/).
- * 
+ *
  * SpaceModule is free software: you can redistribute it and/or modify it under the terms of the
  * Attribution-NonCommercial-ShareAlike Unported (CC BY-NC-SA) license as published by the Creative
  * Common organization, either version 3.0 of the license, or (at your option) any later version.
- * 
- * SpaceBukkit is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ *
+ * SpaceBukkit is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * Attribution-NonCommercial-ShareAlike Unported (CC BY-NC-SA) license for more details.
- * 
+ *
  * You should have received a copy of the Attribution-NonCommercial-ShareAlike Unported (CC BY-NC-SA)
  * license along with this program. If not, see <http://creativecommons.org/licenses/by-nc-sa/3.0/>.
  */
-package me.neatmonster.spacemodule.management;
+package net.xereo.spacemodule.management;
 
 import java.util.LinkedHashMap;
 
-import me.neatmonster.spacemodule.SpaceModule;
-import me.neatmonster.spacemodule.utilities.Console;
-import me.neatmonster.spacemodule.utilities.Utilities;
+import net.xereo.spacemodule.SpaceModule;
+import net.xereo.spacemodule.utilities.Console;
+import net.xereo.spacemodule.utilities.Utilities;
 
 import org.bukkit.util.config.Configuration;
 
