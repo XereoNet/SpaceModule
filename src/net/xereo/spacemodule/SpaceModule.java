99a8710c8b87a6e15fc4b4c3f6f08afef417bd00
drdanick <>
2012-04-07 00:43:56 +1200

    +Cleaned up pom.xml and fixed snakeyaml dependency. #Refactor: me.neatmonster --> net.xereo

--- src/net/xereo/spacemodule/SpaceModule.java
+++ src/net/xereo/spacemodule/SpaceModule.java
@@ -1,18 +1,18 @@
 /*
  * This file is part of SpaceModule (http://spacebukkit.xereo.net/).
- * 
+ *
  * SpaceModule is free software: you can redistribute it and/or modify it under the terms of the
  * Attribution-NonCommercial-ShareAlike Unported (CC BY-NC-SA) license as published by the Creative
  * Common organization, either version 3.0 of the license, or (at your option) any later version.
- * 
- * SpaceBukkit is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ *
+ * SpaceBukkit is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * Attribution-NonCommercial-ShareAlike Unported (CC BY-NC-SA) license for more details.
- * 
+ *
  * You should have received a copy of the Attribution-NonCommercial-ShareAlike Unported (CC BY-NC-SA)
  * license along with this program. If not, see <http://creativecommons.org/licenses/by-nc-sa/3.0/>.
  */
-package me.neatmonster.spacemodule;
+package net.xereo.spacemodule;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,10 +22,10 @@ import java.net.URL;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import me.neatmonster.spacemodule.management.ImprovedClassLoader;
-import me.neatmonster.spacemodule.management.VersionsManager;
-import me.neatmonster.spacemodule.utilities.Console;
-import me.neatmonster.spacemodule.utilities.Utilities;
+import net.xereo.spacemodule.management.ImprovedClassLoader;
+import net.xereo.spacemodule.management.VersionsManager;
+import net.xereo.spacemodule.utilities.Console;
+import net.xereo.spacemodule.utilities.Utilities;
 
 import org.bukkit.util.config.Configuration;
 
